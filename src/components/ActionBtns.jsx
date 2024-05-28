a8173003e60b65999767f3f9d1baee5ccc4f5025
Thanh Tran <tranthanhvi66@gmail.com>
2024-05-29 00:31:45 +0300

    edited img src

--- src/components/ActionBtns.jsx
+++ src/components/ActionBtns.jsx
@@ -4,11 +4,11 @@ import './ActionBtns.css'
 const ActionBtns = () => {
   return (
     <div>
-      <button><img src="./src/assets/eye.png" alt="check-profile" /></button>
-      <button><img src="./src/assets/paper-plane.png" alt="send-inviation" /></button>
-      <button><img src="./src/assets/email.png" alt="send-email" /></button>
-      <button><img src="./src/assets/speech-bubble.png" alt="chat" /></button>
-      <button><img src="./src/assets/block-user.png" alt="block-user" /></button>
+      <button><img src="/src/assets/eye.png" alt="check-profile" /></button>
+      <button><img src="/src/assets/paper-plane.png" alt="send-inviation" /></button>
+      <button><img src="/src/assets/email.png" alt="send-email" /></button>
+      <button><img src="/src/assets/speech-bubble.png" alt="chat" /></button>
+      <button><img src="/src/assets/block-user.png" alt="block-user" /></button>
     </div>
   );
 };
